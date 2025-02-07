b5f3755f1c1fe844fb65e2381d4437ed61764bd3
Adele <2204265@uad.ac.uk>
2025-02-07 17:12:05 +0000

    Basic movement done
    Still missing raycasting to check if grounded, as well as crouching and sliding

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,11 +19,14 @@ public class MovementScript : MonoBehaviour
     //    }
     //    controls.Gameplay.Enable();
     //}
-    [SerializeField] private float runSpeed = 5;
-    [SerializeField] private float acceleration = 2;
-    [SerializeField] private float deceleration = 2;
+
+    [SerializeField] private float runSpeed = 8;
+    [SerializeField] private float acceleration = 20;
+    [SerializeField] private float deceleration = 15;
     [SerializeField] private float jumpStrength = 5;
+    [SerializeField] private float fallActivationSpeed = 1; //At what vertical speed the higher gravity kicks in at
     [SerializeField] private float fallMult = 2;
+    [SerializeField] private float maxFallSpeed = 5;
 
     private bool grounded; //Grounded can be either on the floor or on the wall in theory
 
@@ -35,29 +39,49 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckGrounded();
         JumpAndFall();
         WalkRun();
+    }
+
+    void CheckGrounded() {
+        
     }
 
     void JumpAndFall() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             rb.velocityY = jumpStrength;
         }
-        if (rb.velocityY < 0) {
+        if (rb.velocityY < fallActivationSpeed) {
             rb.velocityY += (fallMult - 1) * Physics2D.gravity.y * Time.deltaTime; //fallmult - 1 since gravity gets applied by default
-        } else if (rb.velocityY > 0 && !Input.GetKey(KeyCode.Space)) {
-            rb.velocityY += (fallMult - 1) * Physics2D.gravity.y * Time.deltaTime; //fallmult - 1 since gravity gets applied by default
-        }
+            if (rb.velocityY < -maxFallSpeed) { //Less than because its negative
+                rb.velocityY = -maxFallSpeed;
+            }
+        } 
+        //else if (rb.velocityY > 0 && !Input.GetKey(KeyCode.Space)) {
+        //    rb.velocityY += (fallMult - 1) * Physics2D.gravity.y * Time.deltaTime; //fallmult - 1 since gravity gets applied by default
+        //}
     }
 
     void WalkRun() {
         if (Input.GetKey(KeyCode.A)) {
-            rb.velocityX += -acceleration; 
+            rb.velocityX += -acceleration * Time.deltaTime;
+            if (Mathf.Sign(rb.velocityX) == 1) {
+                rb.velocityX += deceleration * -rb.velocityX * Time.deltaTime;
+            }
         } else if (Input.GetKey(KeyCode.D)) {
-            rb.velocityX += acceleration;
+            rb.velocityX += acceleration * Time.deltaTime;
+            if (Mathf.Sign(rb.velocityX) == -1) {
+                rb.velocityX += deceleration * -rb.velocityX * Time.deltaTime;
+            }
+        } else if (rb.velocityX != 0){
+            rb.velocityX += deceleration * -rb.velocityX * Time.deltaTime; //Decelerate when not holding left or right
+            if (Mathf.Abs(rb.velocityX) < 0.1) {
+                rb.velocityX = 0;
+            }
         }
         if (Mathf.Abs(rb.velocityX) > runSpeed) {
-            rb.velocityX = runSpeed * rb.velocityX / Mathf.Abs(rb.velocityX); //Sets the speed to either runSpeed or -runSpeed
+            rb.velocityX = runSpeed * Mathf.Sign(rb.velocityX); //Sets the speed to either runSpeed or -runSpeed
         }
     }
 }
