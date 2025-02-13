16998ca75e3458fdd862a50bfecbb874b49852b8
Adele <2204265@uad.ac.uk>
2025-02-13 11:03:18 +0000

    Eugh nasty merge conflict from trying to use maincameramovement instead of main
    Trying to just implement camera following player better when falling as well as proper footsteps

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -15,7 +15,11 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] float exponentX = 2f; //How quickly the camera speeds up with distance horizontally
     [SerializeField] float exponentY = 2f; //How quickly the camera speeds up with distance vertically
     [SerializeField] float directionalOffset = 2f; //How far in front of the player the camera settles at
+    [SerializeField] float runningOffsetMult = 1f; //Multiplies how much the player's velocity affects the cameras position, letting it settle in front of the player
     [SerializeField] float verticalOffset = 2f; //How far above the player the camera settles at
+    [SerializeField] float fallingOffsetMult = 0.1f; //Multiplies how much of the player's falling velocity affects camera position, allowing the player to see below them when falling.
+    [SerializeField] float fallingOffsetThreshold = -2f; //The vertical velocity the player needs to have for the camera to start taking it into the calculations
+    
 
     void Awake()
     {
@@ -29,8 +33,20 @@ public class CameraMovement : MonoBehaviour
         int playerDir = Convert.ToInt32(player.facingRight) * 2 - 1;
         if (MathF.Abs(smoothTurning + playerDir * 0.1f) <= 1f) {
             smoothTurning += playerDir * 0.1f * turningMult;
+
         } 
-        targetPos = player.rb.position + (Vector2.right * smoothTurning * directionalOffset) + Vector2.up * verticalOffset;
+
+        int isPlayerFalling = 0;
+        if (player.rb.velocity.y < fallingOffsetThreshold) {
+            isPlayerFalling = 1;
+        }
+
+        targetPos = player.rb.position
+                    + Vector2.right * smoothTurning * directionalOffset
+                    + Vector2.up * verticalOffset
+                    + Vector2.right * player.rb.velocityX * runningOffsetMult * 0.1f
+                    + Vector2.up * isPlayerFalling * (player.rb.velocityY - fallingOffsetThreshold) * fallingOffsetMult;
+
 
         //rb.velocity += (targetPos - rb.position) * 0.5f;
         if (Vector2.Distance(rb.position, targetPos) < 0.1f) {
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -8,10 +8,23 @@ using UnityEngine.InputSystem;
 public class MovementScript : MonoBehaviour
 {
 
+    public AK.Wwise.Event playerFootstep;
+
+    //The speed at which footstep sounds are triggered. Whenever footstepRate is 1 a footstep is played
+	[SerializeField][Range(0.01f, 3.0f)] private float footstepRate = 1f;
+
+    //How much the velocity of the player affects the footstep frequency
+	[SerializeField][Range(0.01f, 3.0f)] private float footstepRateScaler = 1f;
+
+    //Used to determine when to trigger footstep sounds.
+    private float footstepCount = 0.0f;
+	
+
     [NonSerialized] public Rigidbody2D rb;
+    private BoxCollider2D collider;
     private SpriteRenderer spriteRenderer;
 
-    [SerializeField] private float maxRunSpeed = 8;
+    [SerializeField] private float maxRunSpeed = 8; //The fastest the player can go horizontally
     [SerializeField] private float acceleration = 20; //Speeding up when running
     [SerializeField] private float deceleration = 15; //Slowing down when no longer running / running in opposite direction
     [SerializeField] private float jumpStrength = 5; //Initial vertical velocity when jumping
@@ -43,22 +56,29 @@ public class MovementScript : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         CheckGrounded();
         JumpAndFall();
         WalkRun();
     }
 
     void CheckGrounded() {
-        Vector2 rightRayStart = transform.position + new Vector3(transform.localScale.x * 0.99f / 2f,
-                                                                -transform.localScale.y * 0.99f / 2f);
-        Vector2 leftRayStart = transform.position + new Vector3(-transform.localScale.x * 0.99f / 2f,
-                                                                -transform.localScale.y * 0.99f / 2f);
-        if (Physics2D.Raycast(rightRayStart, Vector2.down, 0.1f, layers) ||
-        Physics2D.Raycast(leftRayStart, Vector2.down, 0.1f, layers)) {
+
+        Vector2 rightRayStart = rb.position + collider.offset + new Vector2(collider.size.x * 0.99f / 2f,
+                                                                           -collider.size.y * 0.99f / 2f);
+        Vector2 leftRayStart = rb.position + collider.offset + new Vector2(-collider.size.x * 0.99f / 2f,
+                                                                           -collider.size.y * 0.99f / 2f);
+
+        if (Physics2D.Raycast(rightRayStart, Vector2.down, 0.15f, layers) ||
+        Physics2D.Raycast(leftRayStart, Vector2.down, 0.15f, layers)) {
+            if (!grounded) {
+                //Plays the Player_Land sound
+                AkSoundEngine.PostEvent("Player_Land", this.gameObject);
+            }
             grounded = true;
         } else {
             grounded = false;
@@ -75,9 +95,6 @@ public class MovementScript : MonoBehaviour
                 rb.velocityY = -maxFallSpeed;
             }
         } 
-        //else if (rb.velocityY > 0 && !Input.GetKey(KeyCode.Space)) {
-        //    rb.velocityY += (fallMult - 1) * Physics2D.gravity.y * Time.deltaTime; //fallmult - 1 since gravity gets applied by default
-        //}
     }
 
     void WalkRun() {
@@ -101,6 +118,15 @@ public class MovementScript : MonoBehaviour
         }
         if (Mathf.Abs(rb.velocityX) > maxRunSpeed) {
             rb.velocityX = maxRunSpeed * Mathf.Sign(rb.velocityX); //Sets the speed to either runSpeed or -runSpeed
+        }
+
+        if (Mathf.Abs(rb.velocityX) > 0.1 && grounded) {
+            footstepCount += (Mathf.Abs(rb.velocityX) * footstepRateScaler) * footstepRate * Time.deltaTime;
+            if (footstepCount > 1) {
+                Debug.Log(rb.position);
+                playerFootstep.Post(gameObject);
+                footstepCount--;
+            }           
         }
         spriteRenderer.flipX = !facingRight;
     }
