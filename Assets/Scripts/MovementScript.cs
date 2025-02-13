57ab1d8aceb37dbca8cc3e364c9a3e6e9fada690
Adele <2204265@uad.ac.uk>
2025-02-13 11:16:39 +0000

    Fixed a small bug I made

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -78,6 +78,7 @@ public class MovementScript : MonoBehaviour
                 //Plays the Player_Land sound
                 AkSoundEngine.PostEvent("Player_Land", this.gameObject);
             }
+            grounded = true;
         } else {
             grounded = false;
         }
