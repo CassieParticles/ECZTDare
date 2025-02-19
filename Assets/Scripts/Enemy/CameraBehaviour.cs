2b697b27a19a260fa52a16301cef1f1745df3b1e
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-02-19 14:58:25 +0000

    Cleaned up code to fit code structure

--- Assets/Scripts/Enemy/CameraBehaviour.cs
+++ Assets/Scripts/Enemy/CameraBehaviour.cs
@@ -17,102 +17,7 @@ public class CameraBehaviour : BaseEnemyBehaviour
     private bool turningCCW;
     private bool turningPaused;
 
-    private void Awake()
-    {
-        Setup();
-        initialAngle = visionCone.transform.rotation.eulerAngles.z;
-        turningCCW = true;
-        turningPaused = maxAngle == 0;
-
-        //Start camera moving sound
-        cameraMoving.Post(gameObject);
-    }
-
-    private void Start()
-    {
-        if(alarm)
-        {
-            alarm.AddAlarmEnableFunc(Alarm);
-            alarm.AddAlarmDisableFunc(AlarmOff);
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        //Handle seeing the player
-        if(Player)
-        {
-            if(suspicion <=100)
-            {
-                suspicion += GetSuspicionIncrease(Player.transform.position) * Time.fixedDeltaTime;
-            }
-            else
-            {
-                //Raise alarm
-                if(alarm && !alarm.AlarmGoingOff())
-                {
-                    enemyAlerted.Post(gameObject);
-                    alarm.StartAlarm(Player.transform.position);
-                }
-            }
-        }
-        else if(suspicion > 0)
-        {
-            if (alarm && !alarm.AlarmGoingOff())
-            {
-                suspicion -= suspicionDecayRate * Time.fixedDeltaTime;
-            }
-            else if(!alarm)
-            {
-                suspicion -= suspicionDecayRate * Time.fixedDeltaTime;
-            }
-        }
-
-
-        //Handle camera rotation
-        float visionAngle = visionCone.transform.rotation.eulerAngles.z;
-        float upperBound = initialAngle + maxAngle * +1;
-        float lowerBound = initialAngle + maxAngle * -1;
-
-        //Case 1: Area between upper and lower bound is continuous
-        if(upperBound < 360 && lowerBound > 0)
-        {
-            if(Mathf.Abs(visionAngle - initialAngle) > maxAngle)
-            {
-                StartCoroutine(ChangeCameraDirection());
-            }
-        }
-        //Case 2: Upper bound goes past 360 degrees
-        else if(upperBound > 360)
-        {
-            if(visionAngle < lowerBound)
-            {
-                upperBound -= 360;
-            }
-            if(visionAngle > upperBound)
-            {
-                StartCoroutine(ChangeCameraDirection());
-            }
-        }
-        //Case 3: Lower bound goes past 0 degrees
-        else
-        {
-            if(visionAngle > upperBound)
-            {
-                lowerBound += 360;
-            }
-            if(visionAngle < lowerBound)
-            {
-                StartCoroutine(ChangeCameraDirection());
-            }
-        }
-
-        //Turn camera
-        if(!turningPaused)
-        {
-            RotateCamera(turnSpeed * Time.fixedDeltaTime * (turningCCW?1:-1));
-        }
-    }
+   
 
     private IEnumerator ChangeCameraDirection()
     {
@@ -150,6 +55,103 @@ public class CameraBehaviour : BaseEnemyBehaviour
     private void AlarmOff()
     {
 
+    }
+
+    private void Awake()
+    {
+        Setup();
+        initialAngle = visionCone.transform.rotation.eulerAngles.z;
+        turningCCW = true;
+        turningPaused = maxAngle == 0;
+
+        //Start camera moving sound
+        cameraMoving.Post(gameObject);
+    }
+
+    private void Start()
+    {
+        if (alarm)
+        {
+            alarm.AddAlarmEnableFunc(Alarm);
+            alarm.AddAlarmDisableFunc(AlarmOff);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Handle seeing the player
+        if (Player)
+        {
+            if (suspicion <= 100)
+            {
+                suspicion += GetSuspicionIncrease(Player.transform.position) * Time.fixedDeltaTime;
+            }
+            else
+            {
+                //Raise alarm
+                if (alarm && !alarm.AlarmGoingOff())
+                {
+                    enemyAlerted.Post(gameObject);
+                    alarm.StartAlarm(Player.transform.position);
+                }
+            }
+        }
+        else if (suspicion > 0)
+        {
+            if (alarm && !alarm.AlarmGoingOff())
+            {
+                suspicion -= suspicionDecayRate * Time.fixedDeltaTime;
+            }
+            else if (!alarm)
+            {
+                suspicion -= suspicionDecayRate * Time.fixedDeltaTime;
+            }
+        }
+
+
+        //Handle camera rotation
+        float visionAngle = visionCone.transform.rotation.eulerAngles.z;
+        float upperBound = initialAngle + maxAngle * +1;
+        float lowerBound = initialAngle + maxAngle * -1;
+
+        //Case 1: Area between upper and lower bound is continuous
+        if (upperBound < 360 && lowerBound > 0)
+        {
+            if (Mathf.Abs(visionAngle - initialAngle) > maxAngle)
+            {
+                StartCoroutine(ChangeCameraDirection());
+            }
+        }
+        //Case 2: Upper bound goes past 360 degrees
+        else if (upperBound > 360)
+        {
+            if (visionAngle < lowerBound)
+            {
+                upperBound -= 360;
+            }
+            if (visionAngle > upperBound)
+            {
+                StartCoroutine(ChangeCameraDirection());
+            }
+        }
+        //Case 3: Lower bound goes past 0 degrees
+        else
+        {
+            if (visionAngle > upperBound)
+            {
+                lowerBound += 360;
+            }
+            if (visionAngle < lowerBound)
+            {
+                StartCoroutine(ChangeCameraDirection());
+            }
+        }
+
+        //Turn camera
+        if (!turningPaused)
+        {
+            RotateCamera(turnSpeed * Time.fixedDeltaTime * (turningCCW ? 1 : -1));
+        }
     }
     private void OnDrawGizmosSelected()
     {
