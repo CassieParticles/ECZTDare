12288f8393c722ed13f1bc4d59100684347fd95d
Rebecca <146950501+Azaersyss@users.noreply.github.com>
2025-03-12 11:32:35 +0000

    Added hacking audio to Wwise

--- Assets/Scripts/Player/Hackable/CameraHackable.cs
+++ Assets/Scripts/Player/Hackable/CameraHackable.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class CameraHackable : Hackable
 {
+    public AK.Wwise.Event Hack_Start;
+    public AK.Wwise.Event Hack_Stop;
+
     [SerializeField] float hackTimer = 5;
     [SerializeField] float hackDetectionRadius = 8;
 
@@ -15,12 +18,15 @@ public class CameraHackable : Hackable
         cameraAttached.beingHacked = true;
         cameraAttached.visionCone.distance = 0;
         beingHacked = true;
+        Hack_Start.Post(gameObject);
 
         yield return new WaitForSeconds(hackTimer);
         //Enable camera
         cameraAttached.beingHacked = false;
         cameraAttached.visionCone.distance = dist;
         beingHacked = false;
+        Hack_Stop.Post(gameObject);
+
     }
     private void Awake()
     {
