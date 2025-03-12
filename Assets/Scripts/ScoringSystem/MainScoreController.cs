e571d25e0b38e016e239a9176f1148d22f55a668
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-12 12:13:47 +0000

    Timer is added to level when it starts

--- Assets/Scripts/ScoringSystem/MainScoreController.cs
+++ Assets/Scripts/ScoringSystem/MainScoreController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainScoreController : MonoBehaviour
 {
@@ -17,6 +18,16 @@ public class MainScoreController : MonoBehaviour
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += ChangeLevel;
+    }
+
+    void ChangeLevel(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "Level1")
+        {
+            StartTimer();
+        }
     }
 
     public static MainScoreController GetInstance()
@@ -29,8 +40,11 @@ public class MainScoreController : MonoBehaviour
         TimerObject = Instantiate(TimerObjectPrefab);
     }
 
+    
+
     public void StopTimer()
     {
         float timeTaken = TimerObject.GetComponent<ScoreTimer>().time;
+        Debug.Log("Time taken: " + timeTaken);
     }
 }
