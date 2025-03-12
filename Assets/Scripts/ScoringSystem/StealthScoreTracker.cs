e22f2b6e2f830c371847cc38207e9a51ad42b075
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-12 15:11:08 +0000

    Scoring system has trackable stealth score that can be reduced by action

---
+++ Assets/Scripts/ScoringSystem/StealthScoreTracker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealthScoreTracker : MonoBehaviour
+{
+    public float score { get; private set; }
+    private void Awake()
+    {
+        score = 120;
+    }
+
+    public void AddScore(float scoreAdd) { score += scoreAdd; }
+    public void RemoveScore(float scoreRemove) { score -= scoreRemove; }
+
+}
