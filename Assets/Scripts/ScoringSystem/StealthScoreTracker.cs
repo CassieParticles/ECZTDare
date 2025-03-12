334518240bf8b8699fcfcdd916c5ffc596516dbb
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-12 15:21:47 +0000

    Stealth score can be reduced by being caught

--- Assets/Scripts/ScoringSystem/StealthScoreTracker.cs
+++ Assets/Scripts/ScoringSystem/StealthScoreTracker.cs
@@ -10,6 +10,10 @@ public class StealthScoreTracker : MonoBehaviour
         score = 120;
     }
 
+    public static StealthScoreTracker GetTracker()
+    {
+        return FindFirstObjectByType<StealthScoreTracker>();
+    }
     public void AddScore(float scoreAdd) { score += scoreAdd; }
     public void RemoveScore(float scoreRemove) { score -= scoreRemove; }
 
