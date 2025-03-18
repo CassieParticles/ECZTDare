f131f956fa3b23cc30b3bd4e7a3a01cd117d3918
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-18 11:43:19 +0000

    Checkpoint script

---
+++ Assets/Scripts/Environment/Checkpoints/CheckpointManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    GameObject[] gameObjects;
+    int upcomingCheckpoint;
+    private void Awake()
+    {
+        
+    }
+}
