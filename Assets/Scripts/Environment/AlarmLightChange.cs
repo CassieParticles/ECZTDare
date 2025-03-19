b9a35e015b5ba700dc417fc5de59d63b3aa0b7be
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-19 12:20:24 +0000

    Set up AlarmLight class to take in object for getting lights

--- Assets/Scripts/Environment/AlarmLightChange.cs
+++ Assets/Scripts/Environment/AlarmLightChange.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class AlarmLightChange : MonoBehaviour
 {
+    [SerializeField] private GameObject alarmLights;
     [SerializeField] private Color alarmColour;
     private Color defaultColour;
 
+    Light2D[] lights;
+
+    AlarmSystem alarmSystem;
+
+    private void AlarmOn(Vector3 playerPosition)
+    {
+        foreach(Light2D light in lights)
+        {
+            light.color = alarmColour;
+        }
+    }
+
+    private void AlarmOff()
+    {
+        foreach (Light2D light in lights)
+        {
+            light.color = defaultColour;
+        }
+    }
+
     private void Awake()
     {
-        
+        alarmSystem = GetComponent<AlarmSystem>();
+
+        //Get attached lights
+        lights = alarmLights.transform.GetComponentsInChildren<Light2D>();
+    }
+
+    private void Start()
+    {
+
+
     }
 }
 
