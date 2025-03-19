c78d290704246507f4eed4bbdbce530e916f8a39
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-19 12:05:22 +0000

    Added alarm change colour script and added to alarm prefab

---
+++ Assets/Scripts/Environment/AlarmLightChange.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmLightChange : MonoBehaviour
+{
+    [SerializeField] private Color alarmColour;
+    private Color defaultColour;
+
+    private void Awake()
+    {
+        
+    }
+}
+
+    
