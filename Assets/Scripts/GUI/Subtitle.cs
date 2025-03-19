a1ef745e2e899e2374026a10d196d032fd86fb8e
Adele <2204265@uad.ac.uk>
2025-03-19 21:16:59 +0000

    Added subtitle prefab and manager

--- Assets/Scripts/GUI/Subtitle.cs
+++ Assets/Scripts/GUI/Subtitle.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class Subtitle : MonoBehaviour
@@ -12,77 +10,120 @@ public class Subtitle : MonoBehaviour
         WriteJarbled
     }
 
-    [SerializeField] private List<string> text;
+    //[SerializeField] private List<string> text;
+    [SerializeField] private string text;
     [SerializeField] private float duration;
     [SerializeField] private TextMode SubtitleMode = TextMode.Instant;
     [SerializeField] private float writingSpeed;
     [SerializeField] private int jarblerSpeed;
 
-    private TextMeshProUGUI textBox;
+    private SubtitleManager subtitleManager;
 
     private bool writing = false;
-    private bool writingCoroutineStarted;
-    public string writtenText = "";
-    private int textNumber = 0;
+    private bool writingCoroutine;
+    [NonSerialized] public string writtenText = "";
+    //private int textNumber = 0;
     private bool finished = false;
     private bool waiting = false;
     private float timer;
 
+    public bool turnOn = false;
+
     private void Awake() {
         //speaker = GameObject.Find("Player").GetComponentInChildren<TextMeshProUGUI>();
-        textBox = GameObject.Find("SubtitleText").GetComponent<TextMeshProUGUI>();
+        subtitleManager = GameObject.Find("SubtitleText").GetComponent<SubtitleManager>();
     }
 
-    public void RunEvent() {
-        float writingDuration = text[textNumber].Length / writingSpeed;
-        timer = writingDuration;
-        writing = true;
+    private void StartSubtitle() {
+        //float writingDuration = text.Length / writingSpeed;
+        //timer = writingDuration;
+        if (!subtitleManager.subtitles.Contains(this)) {
+            writing = true;
+            subtitleManager.AddSubtitle(this);
+        }
     }
 
-    public void EndEvent() {
-        
+    public void EndSubtitle() {
+        if (subtitleManager.subtitles.Contains(this)) {
+            subtitleManager.RemoveSubtitle(this);
+            subtitleManager.UpdateText();
+            writing = false;
+            finished = false;
+            waiting = false;
+            timer = 0;
+            writtenText = "";
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (writing && !writingCoroutineStarted && timer >= 0 && !finished) {
+        if (turnOn) {
+            turnOn = false;
+            if (!writing || !finished || !waiting) {
+                StartSubtitle();
+            }
+        }
+
+        if (writing && !writingCoroutine && timer >= 0 && !finished) {
             if (SubtitleMode != TextMode.Instant) {
                 StartCoroutine(WriteText());
             } else {
-                writtenText = text[textNumber];
+                writtenText = text/*[textNumber]*/;
+                subtitleManager.UpdateText();
+                writing = false;
                 finished = true;
+                timer = duration;
+                waiting = true;
             }
-        } else if (finished && !waiting && timer <= 0) {
+        } else if (finished && !waiting && !writingCoroutine) {
             timer = duration;
             waiting = true;
         }
-        duration -= Time.deltaTime;
-        if (duration <= 0 && finished && !waiting) {
-            EndEvent();
+        if (timer > 0 && (writing | finished)) {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0 && finished && waiting) {
+            EndSubtitle();
         }
     }
 
     IEnumerator WriteText() {
-        writingCoroutineStarted = true;
+        writingCoroutine = true;
+        string textSoFar = "";
+        int iterationsPerFrame = 1;
         if (SubtitleMode == TextMode.WriteJarbled) {
-            string textSoFar = "";
-            for (int i = 0; i < text[textNumber].Length; i++) {
+            for (int i = 0; i < text.Length; i++) {
                 for (int j = 0; j < jarblerSpeed; j++) {
-                    writtenText = textSoFar + Convert.ToChar(UnityEngine.Random.Range(21, 126));
-                    yield return new WaitForSeconds(1 / (writingSpeed * jarblerSpeed));
+                    writtenText = textSoFar + Convert.ToChar(UnityEngine.Random.Range(21, 122));
+                    subtitleManager.UpdateText();
+                    if (1f / writingSpeed * jarblerSpeed * iterationsPerFrame < 1f/60f) {
+                        iterationsPerFrame++;
+                    } else {
+                        iterationsPerFrame = 1;
+                        yield return new WaitForSeconds(1 / (writingSpeed * jarblerSpeed));
+                    }
                 }
-                textSoFar += text[textNumber][i];
+                textSoFar += text[i];
             }
+            writtenText = textSoFar;
+            subtitleManager.UpdateText();
         } else {
-            for (int i = 0; i < text[textNumber].Length; i++) {
-                    writtenText += text[textNumber][i];
+            for (int i = 0; i < text.Length; i++) {
+                textSoFar += text[i];
+                writtenText = textSoFar;
+                subtitleManager.UpdateText();
+                if (1f / writingSpeed * iterationsPerFrame < 1f / 60f) {
+                    iterationsPerFrame++;
+                } else {
+                    iterationsPerFrame = 1;
                     yield return new WaitForSeconds(1 / writingSpeed);
+                }
             }
         }
-        textNumber++;
+        //textNumber++;
         writing = false;
-        writingCoroutineStarted = false;
+        writingCoroutine = false;
         finished = true;
     }
 }
--- Assets/Scripts/GUI/SubtitleManager.cs
+++ Assets/Scripts/GUI/SubtitleManager.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SubtitleManager : MonoBehaviour
 {
-
-    public void AddSubtitle() {
-
+    public List<Subtitle> subtitles;
+    public TextMeshProUGUI textBox;
+    public string currentSubtitles;
+    public void AddSubtitle(Subtitle subtitle) {
+        subtitles.Add(subtitle);
     }
+
+    public void RemoveSubtitle(Subtitle subtitle) {
+        subtitles.Remove(subtitle);
+    }
+
+    public void UpdateText() {
+        currentSubtitles = "";
+        if (subtitles.Count != 0) {
+            foreach (Subtitle subtitle in subtitles) {
+                currentSubtitles += subtitle.writtenText + "\n";
+            }
+        }
+        textBox.text = currentSubtitles;
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        textBox = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        //if (subtitles.Count != 0) {
+        //    foreach (Subtitle subtitle in subtitles) {
+        //        currentSubtitles += subtitle.writtenText + "\n";
+        //    }
+       // }
+        //textBox.text = currentSubtitles;
     }
 }
