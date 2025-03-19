dfaaf96b8f9eddc7ed6ff331e058b09c3bb38891
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-19 15:26:28 +0000

    Added Cloak and Sprint hackables

---
+++ Assets/Scripts/Player/Hackable/SprintConsoleHackable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprintConsoleHackable : Hackable
+{
+    public override void OnHack()
+    {
+        //Unlock sprint
+    }
+}
