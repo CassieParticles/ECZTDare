f6b31e313e0fbd41a6f1d73167094c5ef165601f
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-20 16:33:20 +0000

    Cutscene can be moved

--- Assets/Cutscene/EndCutscene/StartCutScene.cs
+++ Assets/Cutscene/EndCutscene/StartCutScene.cs
@@ -42,6 +42,7 @@ public class StartCutScene : MonoBehaviour
             //Wait for player to be grounded
             waitForGrounded = true;
             player = playerScript;
+            player.transform.parent = GameObject.Find("Cutscene").transform;
         }
     }
 }
