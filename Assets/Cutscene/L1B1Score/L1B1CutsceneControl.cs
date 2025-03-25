496682e39013849ebaea22db009f6394292c6199
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-25 13:31:13 +0000

    Cutscene can now play animation using instantiated gameobject

--- Assets/Cutscene/L1B1Score/L1B1CutsceneControl.cs
+++ Assets/Cutscene/L1B1Score/L1B1CutsceneControl.cs
@@ -1,23 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class L1B1CutsceneControl : MonoBehaviour
 {
     [SerializeField] GameObject ScoreGUIPrefab;
 
     GameObject ScoreGUI;
+    PlayableDirector director;
+
+    private void Awake()
+    {
+        director=GetComponent<PlayableDirector>();
+        DisplayScore(30, 70);
+    }
 
     public void DisplayScore(float timeTaken, int stealthScore)
     {
         //Create GUI and get text
         ScoreGUI = Instantiate(ScoreGUIPrefab);
-        GameObject TimeScoreText = ScoreGUI.transform.GetChild(1).gameObject;
-        GameObject StealthScoreText = ScoreGUI.transform.GetChild(2).gameObject;
+        GameObject TimeScoreText = ScoreGUI.transform.GetChild(3).gameObject;
+        GameObject StealthScoreText = ScoreGUI.transform.GetChild(4).gameObject;
 
         //Update text with score
-        TimeScoreText.GetComponent<TextMeshProUGUI>().text += timeTaken.ToString();
-        StealthScoreText.GetComponent<TextMeshProUGUI>().text += stealthScore.ToString();
+        TimeScoreText.GetComponent<TextMeshProUGUI>().text = timeTaken.ToString();
+        StealthScoreText.GetComponent<TextMeshProUGUI>().text = stealthScore.ToString();
+
+        //Set up Bindings
+        TimelineAsset timeline = director.playableAsset as TimelineAsset;
+        foreach (TrackAsset item in timeline.GetOutputTracks())
+        {
+            Debug.Log(item.name);
+            switch (item.name)
+            {
+                case "Title":
+                    director.SetGenericBinding(item, ScoreGUI.transform.GetChild(0).gameObject);
+                    break;
+                case "TimeTaken":
+                    director.SetGenericBinding(item, ScoreGUI.transform.GetChild(1).gameObject);
+                    break;
+                case "Speed":
+                    director.SetGenericBinding(item, ScoreGUI.transform.GetChild(2).gameObject);
+                    break;
+
+                case "StealthScore":
+                    director.SetGenericBinding(item, ScoreGUI.transform.GetChild(3).gameObject);
+                    break;
+                case "Stealth":
+                    director.SetGenericBinding(item, ScoreGUI.transform.GetChild(4).gameObject);
+                    break;
+            }
+        }
+
+        //Start cutscene
+        director.Play();
     }
 }
