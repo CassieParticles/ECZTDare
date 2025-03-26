d0f68550cec70e8564ff33d78a993ad9005d7cf3
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-03-26 15:08:01 +0000

    Added Parallax to bg

--- Assets/Scripts/Environment/ParallaxScript.cs
+++ Assets/Scripts/Environment/ParallaxScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ParallaxScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //How much the parallax effects
+    [SerializeField,Range(0,1)] float parallaxPower = 0.5f;
+    
+    //Camera being followed
+    GameObject camera;
+
+    //Initial information
+    float startX;
+    float camStartX;
+
+    private void Awake()
     {
-        
+        //Get camera
+        camera = Camera.main.gameObject;
+
+        startX = transform.position.x;
+        camStartX = camera.transform.position.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Get how far the camera has moved
+        float deltaFromStart = ((camera.transform.position.x - camStartX) * parallaxPower);
+
+        Vector3 newPos = transform.position;
+        newPos.x = startX+deltaFromStart;
+        transform.position = newPos;
     }
 }
