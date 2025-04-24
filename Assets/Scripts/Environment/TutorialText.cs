6b3d5216e772a80280cf0be0b7ca52cc353086cf
Rebecca <146950501+Azaersyss@users.noreply.github.com>
2025-04-24 15:30:02 +0100

    Fixed tut text script again

--- Assets/Scripts/Environment/TutorialText.cs
+++ Assets/Scripts/Environment/TutorialText.cs
@@ -19,7 +19,7 @@ public class TutorialText : MonoBehaviour
         hackText = transform.Find("Hack").GetComponent<TextMeshPro>();
 
         jumpText.text = "Press " + jump + " to Jump";
-        slideText.text = "Hold " + slide + " while running to Slide";
+        slideText.text = "Hold " + slide + " While Running to Slide";
         hackText.text = "Aim Hack with the Mouse \n\n Press " + hack + " to Hack Red Doors";
     }
 }
