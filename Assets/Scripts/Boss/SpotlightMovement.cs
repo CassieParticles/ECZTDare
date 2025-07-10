3e67d8ed59ee10b55a7b6a1352f6f2d9610ee3d5
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-07-10 11:20:23 +0100

    Created basic spotlight prefab and script to move around the scene

---
+++ Assets/Scripts/Boss/SpotlightMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SpotlightMovement : MonoBehaviour
+{
+    [SerializeField]private float travelTime = 2.0f;
+
+    private Vector2 desiredLocation;
+    private Vector2 velocity;
+
+    public void MoveTo(Vector2 location)
+    {
+        desiredLocation = location;
+    }
+
+    private void FixedUpdate()
+    {
+        //If spotlight needs to move to new location
+        if ((Vector2)transform.position != desiredLocation)
+        {
+            //Get distance and direction
+            Vector2 toMove = desiredLocation - (Vector2)transform.position;
+
+            if(toMove.magnitude < Time.fixedDeltaTime)
+            {
+                transform.position = (Vector3)desiredLocation;
+            }
+
+            transform.position += ((Vector3)toMove * Time.fixedDeltaTime) / travelTime;
+        }
+    }
+}
