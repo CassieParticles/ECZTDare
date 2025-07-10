cbd02436380deb420df8696642b6131296c07dc0
Adele <2204265@uad.ac.uk>
2025-07-10 11:38:58 +0100

    Implemented crouch (Incomplete due to needing animations)
    I need boti's crouch animations in the game to properly resize the hitbox
    Known bug is being able to completely avoid guards due to crouching below their legs

--- Assets/Scripts/Player/Movement/Sliding.cs
+++ Assets/Scripts/Player/Movement/Sliding.cs
@@ -24,20 +24,39 @@ public class Sliding
         player.effectiveDeceleration = player.slideDeceleration;
         player.tempGroundedTimer = 0.02f;
     }
-    public void StopSliding() {
+    public void StandUp() {
         //Stops the slide sound.
         player.playerSlide.Stop(player.gameObject);
         player.sliding = false;
+        player.crouching = false;
         player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + player.colliderSize.y * 0.31f); //Lower the player so they arent midair when sliding
         player.collider.size = player.colliderSize;
         player.effectiveDeceleration = player.deceleration;
+        player.effectiveAcceleration = player.acceleration;
+        player.effectiveMaxRunSpeed = player.maxRunSpeed;
     }
     public void WhileSliding() {
         //Sets the RTPC Value of horizontalVelocity to the horizontalVelocity float value.
         AkSoundEngine.SetRTPCValue("horizontalVelocity", player.horizontalVelocity);
     }
 
-    public void CanStopSliding() {
+    public void Crouch() {
+        player.crouching = true;
+        player.hasSlid = true;
+        player.collider.size = new Vector2(player.colliderSize.x * 1.5f, player.colliderSize.y * 0.3f);
+        player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y - player.colliderSize.y * 0.31f); //Lower the player so they arent midair when crouching
+        player.rb.velocityY = 0f;
+        player.effectiveDeceleration = player.crouchDeceleration;
+        player.effectiveAcceleration = player.crouchAcceleration;
+        player.effectiveMaxRunSpeed = player.maxCrouchSpeed;
+        player.tempGroundedTimer = 0.02f;
+    }
 
+    public void SlideToCrouch() {
+        player.playerSlide.Stop(player.gameObject);
+        player.sliding = false;
+        player.crouching = true;
+        player.effectiveDeceleration = player.crouchDeceleration;
+        player.effectiveAcceleration = player.crouchAcceleration;
     }
 }
