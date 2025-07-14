1f80338cb77a79b4de836eaf3226e8d242767544
Adele <2204265@uad.ac.uk>
2025-07-14 13:59:33 +0100

    Cleaned up ChaseCamera and made it easier to understand
    Not gonna do more with it rn but can do more later if needed

--- Assets/Scripts/Player/ChaseCamera.cs
+++ Assets/Scripts/Player/ChaseCamera.cs
@@ -8,37 +8,28 @@ public class ChaseCamera : MonoBehaviour
 {
 
     CinemachineVirtualCamera virtualCamera;
-    CinemachineTargetGroup targetGroup;
-    BoxCollider2D trigger;
     PolygonCollider2D track;
     CinemachineBrain mainCamera;
     GameObject player;
-    Rigidbody2D playerRB;
 
+    [Header("SETTINGS")]
     [SerializeField] private CinemachineBlendDefinition.Style blendType = CinemachineBlendDefinition.Style.HardOut;
     [SerializeField][Range(0.1f, 5f)] private float blendDuration = 1;
-    [SerializeField][Range(0.1f, 5f)] private float resetTime = 0.51f;
-    [SerializeField][Range(0f, 20f)] private float playerBias = 1; //If the camera points more towards the player or the point of interest. 0 is point of interest 20 is player
     [SerializeField][Range(5f, 20f)] private float zoom = 8.44f; //Perspective 79 calculations to solve ortho to fov in start
+    [Header("ANCHOR POINTS\nTwo anchors is enough for a straight line\nPosition is relative to the gameobject")]
     [SerializeField] private List<Vector2> anchorPoints = new List<Vector2> { Vector2.zero, Vector2.right };
 
-    private Vector2 origin;
-    private Vector2 offset;
     private float fov;
 
 
     void Start()
     {
         virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
-        trigger = GetComponent<BoxCollider2D>();
         track = GetComponentInChildren<PolygonCollider2D>();
         player = GameObject.Find("Player");
-        playerRB = player.GetComponent<Rigidbody2D>();
         mainCamera = GameObject.Find("Main Camera").GetComponent<CinemachineBrain>();
 
         virtualCamera.Priority = 9;
-        origin = transform.position;
-        offset = trigger.offset;
 
         fov = 2 * Mathf.Rad2Deg * Mathf.Atan(zoom / 10);
         virtualCamera.m_Lens.ModeOverride = LensSettings.OverrideModes.Perspective;
@@ -58,15 +49,7 @@ public class ChaseCamera : MonoBehaviour
         if (collision == player.GetComponent<Collider2D>())
         {
             virtualCamera.Priority = 9;
-            StartCoroutine(WaitThenReset(resetTime));
-        }
-    }
-
-    IEnumerator WaitThenReset(float seconds) {
-        yield return new WaitForSeconds(seconds);
-        if (virtualCamera.Priority == 9 ) {
-            transform.position = origin;
-            trigger.offset = offset;
+            //StartCoroutine(WaitThenReset(resetTime));
         }
     }
 
@@ -78,6 +61,8 @@ public class ChaseCamera : MonoBehaviour
     }
 
     private void OnValidate() {
+        fov = 2 * Mathf.Rad2Deg * Mathf.Atan(zoom / 10);
+        virtualCamera.m_Lens.FieldOfView = fov;
         track = GetComponentInChildren<PolygonCollider2D>();
         List<Vector2> newTrack = new List<Vector2>();
         newTrack.Add(anchorPoints[0]);
