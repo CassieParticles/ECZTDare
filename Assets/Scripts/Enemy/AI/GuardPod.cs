bb83562141dc2972b12bff053fc31e5d3329ba18
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-07-17 13:42:07 +0100

    Created guard pod script

---
+++ Assets/Scripts/Enemy/AI/GuardPod.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Guard pods are a kind of door, just not for the player
+public class GuardPod : BaseDoor
+{
+    
+    public override void Lock()
+    {
+        if (isLocked){ return; }
+        //Tell the guard to return to base
+        isLocked = true;
+    }
+
+    public override void Unlock()
+    {
+        if(isLocked){ return; }
+        //Enable the guard to wander about
+        isLocked = false;
+    }
+
+    public override void ToggleState()
+    {
+        //Toggle between the states
+        isLocked = !isLocked;
+    }
+}
