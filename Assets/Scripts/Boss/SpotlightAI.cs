d7815f5dbde90e5a82a73bb91e5ae78b6f0bd332
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-07-18 10:23:26 +0100

    Spotlight can detect when player is visible

--- Assets/Scripts/Boss/SpotlightAI.cs
+++ Assets/Scripts/Boss/SpotlightAI.cs
@@ -8,6 +8,15 @@ public class SpotlightAI : MonoBehaviour,IRecieveSignals
     SpotlightMovement spotlight;
     BaseAnchor currentAnchor;
 
+    MovementScript Player;
+    private bool playerDetected
+    {
+        get
+        {
+            return Player && Player.cloaking;
+        }
+    }
+
     private void Awake()
     {
         spotlight = GetComponent<SpotlightMovement>();
@@ -22,5 +31,31 @@ public class SpotlightAI : MonoBehaviour,IRecieveSignals
 
         anchor.AddSpotlight(spotlight);
         currentAnchor = anchor;
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerDetected)
+        {
+            Debug.Log("Player visible");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //get if player
+        if(collision.gameObject.GetComponent<MovementScript>())
+        {
+            Player=collision.gameObject.GetComponent<MovementScript>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //get if player
+        if (collision.gameObject.GetComponent<MovementScript>())
+        {
+            Player = null;
+        }
     }
 }
