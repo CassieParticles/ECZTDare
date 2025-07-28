c66efdc3681d4db63418f8a6d5ca9ac877ba0f89
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-07-28 15:42:30 +0100

    Cutscene object needs audio added

--- Assets/Scripts/Cutscene/CutsceneAnim.cs
+++ Assets/Scripts/Cutscene/CutsceneAnim.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class CutsceneAnim : MonoBehaviour
@@ -8,6 +12,32 @@ public class CutsceneAnim : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<VideoPlayer>().Play();
+        //Get video player and start playing video
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.Play();
+        //================= REBECCA ANIMATION STARTS PLAYING HERE ========================//
+
+
+        //================= REBECCA ANIMATION STARTS PLAYING HERE ========================//
+        InputSystem.onAnyButtonPress.CallOnce(ExitScene);
+        player.loopPointReached += EndReached;
     }
+
+    private void ExitScene(InputControl control)
+    {
+        //================= REBECCA SCENE EXITS HERE ======================================//
+
+
+        //================= REBECCA SCENE EXITS HERE ======================================//
+        SceneManager.LoadScene(0);
+    }
+
+    private void EndReached(VideoPlayer vp)
+    {
+        //================= REBECCA ANIMATION STOPS PLAYING HERE ==========================//
+
+
+        //================= REBECCA ANIMATION STOPS PLAYING HERE ==========================//
+    }
+
 }
