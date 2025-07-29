eb8d1951ea498fb82643321e3c759c7f73dccc77
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-07-29 14:40:23 +0100

    Created Lookahead system scripts and prefab

---
+++ Assets/Scripts/GUI/LookAheadSystem/LookaheadTracker.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookaheadTracker : MonoBehaviour
+{
+    public float maxDistance;
+    public Sprite displaySprite;
+    public bool TrackLeft;
+    public bool TrackRight;
+}
