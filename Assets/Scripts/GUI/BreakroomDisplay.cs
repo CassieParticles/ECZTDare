af6bda5017280a44efda31f9094419f4ca6bdc2d
Adele <2204265@uad.ac.uk>
2025-07-30 16:52:47 +0100

    Added Letter Scores that display in breakroom

--- Assets/Scripts/GUI/BreakroomDisplay.cs
+++ Assets/Scripts/GUI/BreakroomDisplay.cs
@@ -24,8 +24,10 @@ public class BreakroomDisplay : MonoBehaviour
 
     public List<ScoreData> scores;
     public List<string> scoresText;
+    public List<string> lettersText;
     public List<SectionStrings> sectionText;
     [SerializeField] TextMeshProUGUI TextBox;
+    [SerializeField] TextMeshProUGUI LetterTextbox;
 
     private string sectionNumberText;
     private string sectionTypeText;
@@ -47,9 +49,11 @@ public class BreakroomDisplay : MonoBehaviour
         scores = importedScores;
         scoresText = new List<string>();
         sectionText = new List<SectionStrings>();
+        lettersText = new List<string>();
 
         ScoreData score = scores[scores.Count - 1];
 
+        ScoreManager scoreManager = ScoreManager.GetInstance();
 
         string formattedText = "";
 
@@ -65,23 +69,28 @@ public class BreakroomDisplay : MonoBehaviour
                 tempSectionString.sectionTypeText = "Chase";
                 tempSectionString.scoreUnitText = "Time: ";
                 tempSectionString.scoreValue = FormatTime(scores[i].chaseTimeSeconds);
+                lettersText.Add(scoreManager.TimeToLetter(scores[i].chaseTimeSeconds) + "\n\n");
 
             } else if (scores[i].stealthSection) {
                 tempSectionString.sectionTypeText = "Stealth";
                 tempSectionString.scoreUnitText = "Score: ";
                 tempSectionString.scoreValue = FormatStealth(scores[i].stealthScore);
-
+                lettersText.Add(scoreManager.ScoreToLetter(scores[i].stealthScore) + "\n\n");
             }
             sectionText[i] = tempSectionString;
 
             formattedText = string.Format("{0}\n{1}\n{2}{3}\n\n", tempSectionString.sectionNumberText, tempSectionString.sectionTypeText, tempSectionString.scoreUnitText, tempSectionString.scoreValue);
             scoresText.Add(formattedText);
+
+            
+
+            
         }
 
 
         
 
-        
+
         StartCoroutine(DisplayAnimation(scores.Count - 1));
 
     }
@@ -91,6 +100,7 @@ public class BreakroomDisplay : MonoBehaviour
         TextBox.text = "";
         for (int i = 0; i < scoresText.Count && i < index; i++) {
             TextBox.text += scoresText[i];
+            LetterTextbox.text += lettersText[i];
         }
         //Display the score piece by piece
         yield return new WaitForSeconds(scoreDisplayDelay);
@@ -101,9 +111,12 @@ public class BreakroomDisplay : MonoBehaviour
         TextBox.text += sectionText[scores.Count - 1].scoreUnitText;
         yield return new WaitForSeconds(scoreDisplayGapDuration);
 
+        ScoreManager scoreManager = ScoreManager.GetInstance();
+
         //Scale up the score until it reaches the achieved score
         float scalingTimer = 0;
         string tempTextboxText = TextBox.text;
+        string tempLetterText = LetterTextbox.text;
         if (scores[scores.Count - 1].chaseSection) {
             //Setup for scaling the time
             float time = scores[scores.Count - 1].chaseTimeSeconds;
@@ -111,12 +124,15 @@ public class BreakroomDisplay : MonoBehaviour
 
             while (scalingTimer < scoreScalingDuration) {
                 float scalingProgress = scalingTimer / scoreScalingDuration;
-                TextBox.text = tempTextboxText + FormatTime(Mathf.Lerp(startTime, time, scalingProgress));
+                float scaledTime = Mathf.Lerp(startTime, time, scalingProgress);
+                TextBox.text = tempTextboxText + FormatTime(scaledTime);
+                LetterTextbox.text = tempLetterText + scoreManager.TimeToLetter(scaledTime);
 
                 scalingTimer += Time.deltaTime;
                 yield return new WaitForFixedUpdate();
             }
             TextBox.text = tempTextboxText + FormatTime(time);
+            LetterTextbox.text = tempLetterText + scoreManager.TimeToLetter(time);
 
         } else if (scores[scores.Count - 1].stealthSection) {
             //Setup for scaling the stealth score
@@ -125,12 +141,15 @@ public class BreakroomDisplay : MonoBehaviour
 
             while (scalingTimer < scoreScalingDuration) {
                 float scalingProgress = scalingTimer / scoreScalingDuration;
-                TextBox.text = tempTextboxText + FormatStealth(Mathf.CeilToInt(Mathf.Lerp(startScore, score, scalingProgress)));
+                int scaledScore = Mathf.CeilToInt(Mathf.Lerp(startScore, score, scalingProgress));
+                TextBox.text = tempTextboxText + FormatStealth(scaledScore);
+                LetterTextbox.text = tempLetterText + scoreManager.ScoreToLetter(scaledScore);
 
                 scalingTimer += Time.deltaTime;
                 yield return new WaitForFixedUpdate();
             }
             TextBox.text = tempTextboxText + FormatStealth(score);
+            LetterTextbox.text = tempLetterText + scoreManager.ScoreToLetter(score);
         }
         
 
